d33c21cce789854ce686804ebac77acb3725bea3
Steve Locke <tap3stry@gmail.com>
2020-03-03 18:58:51 -0500

    Adding new class ExtremeDivision with a constructor

---
+++ src/main/java/org/springframework/samples/petclinic/ExtremeDivision.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic;
+
+public class ExtremeDivision {
+
+	private int x;
+	private int y;
+
+	public ExtremeDivision(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}

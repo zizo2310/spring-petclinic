bd4fafa80af908347750214f8eb2b2e6b2806609
Steve Locke <tap3stry@gmail.com>
2020-03-03 18:56:13 -0500

    Adding new class ExtremeAddition with a constructor

---
+++ src/main/java/org/springframework/samples/petclinic/ExtremeAddition.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic;
+
+public class ExtremeAddition {
+
+	private int x;
+	private int y;
+
+	public ExtremeAddition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}

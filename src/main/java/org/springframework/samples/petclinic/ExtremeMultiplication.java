8364cb9688627163d125d5ba82d9d0e1a23df69c
Steve Locke <tap3stry@gmail.com>
2020-03-03 18:57:05 -0500

    Adding new class ExtremeMultiplication with a constructor

---
+++ src/main/java/org/springframework/samples/petclinic/ExtremeMultiplication.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic;
+
+public class ExtremeMultiplication {
+
+	private int x;
+	private int y;
+
+	public ExtremeMultiplication(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}

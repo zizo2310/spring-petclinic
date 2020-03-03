862aa8de199bbdd05f392c74e1d5f412b9afbd9c
Steve Locke <tap3stry@gmail.com>
2020-03-03 18:57:58 -0500

    Adding new class ExtremeSubtraction with a constructor

---
+++ src/main/java/org/springframework/samples/petclinic/ExtremeSubtraction.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic;
+
+public class ExtremeSubtraction {
+
+	private int x;
+	private int y;
+
+	public ExtremeSubtraction(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}

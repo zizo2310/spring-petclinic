7993649285270dcf3f4a275bdd6bfbda17bebf07
Steve Locke <tap3stry@gmail.com>
2020-03-03 19:13:58 -0500

    Added test case for ExtremeDivision class and extremeDivide method

---
+++ src/test/java/org/springframework/samples/petclinic/ExtremeDivisionTest.java
@@ -0,0 +1,14 @@
+package org.springframework.samples.petclinic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ExtremeDivisionTest {
+
+	 @Test
+	   public void testExtremeDivide() {	
+
+	     ExtremeDivision e1 = new ExtremeDivision(16, 4);
+	      assertEquals(e1.extremeDivide(), 4);     
+	   }
+}

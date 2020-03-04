69c841972b60c2db78047d7e7d06fd35c2d359ae
Steve Locke <tap3stry@gmail.com>
2020-03-03 19:06:13 -0500

    Adding implementation for profoundMathStatment to ExtremeSubtraction class now implementing MathStatement interface

--- src/main/java/org/springframework/samples/petclinic/ExtremeSubtraction.java
+++ src/main/java/org/springframework/samples/petclinic/ExtremeSubtraction.java
@@ -1,6 +1,6 @@
 package org.springframework.samples.petclinic;
 
-public class ExtremeSubtraction {
+public class ExtremeSubtraction implements MathStatement {
 
 	private int x;
 	private int y;
@@ -12,5 +12,10 @@ public class ExtremeSubtraction {
 	
 	public int extremeSubtract() {
 		return this.x - this.y;
+	}
+	
+	@Override
+	public String profoundMathStatement() {
+		return "2 - 2 is also 4 as well!? No just kidding. It's 0";
 	}
 }
